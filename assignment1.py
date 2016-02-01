da6c180355d8661c1e2863011b50cbf12336c44b
cmis <cmis@cmis-hs07.(none)>
2016-02-01 13:33:11 +0700

    first assignment done

---
+++ assignment1.py
@@ -0,0 +1,19 @@
+myName = "Yumi" #my first name
+myAge = 17.75 #my age in years 
+myHeight = 1.66 #my height in meters
+lengthS = 3 #length of the square in units
+lengthR = 5 #length of the rectangle in units
+heightR = 2 #height of the rectangle in units
+monthAge = myAge * 12 #My age in months
+womanaveragelife = 74 #women's average lifespan in years
+yearstolive = womanaveragelife - myAge #To calculate how many years I can live, I subtract my age from the average women's lifespan
+heightFeet = myHeight * 3.28 #Because 1 meter is 3.28 feet, to find my height in feet, I multiply my height by 3.28
+averageheight = 1.60 #The average height in metersof my age and sex in my country
+myHeightinK = myHeight - averageheight #The difference between my height and the average height of my age and sex in my country can be determined by subtracting my height and the average height together
+areaofsquare = lengthS ** 2 #The equation to find the area of a square is square of length so I use 2 as exponents
+voclumecube = lengthS ** 3 / 2 #A volume of a square is length power of 3 so I use exponent of 3 and divide it by 2 so I can find the half of volume of the square
+areaofR = lengthR * heightR / 9 #Area of the rectangle can be determined by multiplying the height and length together and you can divide it by 9 to determine the 1/9 of the area of a rectangle
+print "I am " +str(myAge)+ " years old this year. I have approximately " +str(yearstolive)+ " years to live from now on. Average height in my counry is " +str(averageheight)+ " meters for my age and sex. I am " +str(myHeightinK)+ " meters taller than the average height of my age and sex in my home country.My height in feet is " +str(heightFeet)+ " feet."
+print "Hello. My name is", myName,". I am", myAge, "years old so therefore I have", yearstolive, "years to live. If I have a square with a length of", lengthS, "units, the area of square would be", areaofsquare,"unit squared. On the other hand, if I have a rectangle with a height of", heightR,"units and a length of", lengthR,"units, then 1/9 the area of the rectangle would be", areaofR, "unit squared."
+winkyFace = ";)" #Winky faces
+print winkyFace * 10000
