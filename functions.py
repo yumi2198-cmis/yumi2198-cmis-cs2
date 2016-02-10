b9d2f19b1ea0e4905799f65c0b52b03f52a00810
cmis <cmis@cmis-hs07.(none)>
2016-02-10 10:56:00 +0700

    second

---
+++ functions.py
@@ -0,0 +1,26 @@
+def add(a, b):
+    return a + b
+   
+add (3, 4)
+
+def sub(a, b):
+    return a - b
+
+add (5,3)
+
+def mul(a, b):
+    return a * b
+
+mul (4, 4)
+
+def div(a, b):
+    return a / b
+
+div (2, 3)
+
+def hours_from_seconds (a):
+    return a * 3600 
+
+hours_from_seconds (86400)
+    
+
