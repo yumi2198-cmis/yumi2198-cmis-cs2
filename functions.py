e3509a494b2db16ded43b3bbf56d4cb531759737
cmis <cmis@cmis-hs07.(none)>
2016-02-19 13:50:26 +0700

    functions.what

--- functions.py
+++ functions.py
@@ -30,19 +30,35 @@ def circle_area (a): #the variable represents the radius of a circle
 circle_area (5)
 
 def sphere_volume (a): #the variable represents the radius of a circle 
-	return (4/3)*math.pi*(a**3) #to find a volume of a sphere, you multiplied 4/3 and p with the cubed radius
+    return (4/3)*math.pi*(a**3) #to find a volume of a sphere, you multiplied 4/3 and p with the cubed radius
 
 sphere_volume (5)
  
 def avg_volume (a, b): #the two variables show two distinct radius of two different circles
-	return ((((4/3))*math.pi*(a**3)) + ((4/3)*math.pi*(b**3))) / 2 #to find the average volume of two spheres, you need to find the volume of sphere 1 with the "a" variable and then the volume of sphere 2. Once done, you add the volumes of two different sphere together and then divide by 2 for the average number.
+    return ((((4/3))*math.pi*(a**3)) + ((4/3)*math.pi*(b**3))) / 2 #to find the average volume of two spheres, you need to find the volume of sphere 1 with the "a" variable and then the volume of sphere 2. Once done, you add the volumes of two different sphere together and then divide by 2 for the average number.
 
 avg_volume (10, 20)
 
 def area (a, b, c):
-	return (1/2)*a*b*math.sin(c)
+    s = (a + b + c) /2
+    return (math.sqrt(float((s(s-a)(s-b)(s-c))))
 
-area (1, 2, 2.5)
+def right_align(a):
+    print(" "*80 - len(a))
 
-def right_align (name):
-	return 
+def center (a):
+    print (" "*40 - len(a))
+
+def msg_box(txt):
+    return "+" + ((len(txt)+4)*"-") + "+" + "\n" + "|" + (txt) + (2*" ") + "|" + "\n" + "+" + ((len(txt) +4)*"-") + "+"
+
+a = add(3, 4)
+b = sub(5, 3)
+c = mul(4, 4)
+d = div(2, 3)
+e = hours_from_seconds (86400)
+f = circle_area(5)
+g = sphere_volume(5)
+h = avg_volume (10, 20)
+i = area(1, 2, 2.5)
+j = right_align ("Hello")
