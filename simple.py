029b22b4950ad3db4b1e750e7008c6963df72229
cmis <cmis@cmis-hs07.(none)>
2016-03-07 10:47:39 +0700

    Final chane

--- simple.py
+++ simple.py
@@ -1,13 +1,13 @@
 import math
-def multiply(x):
-    a = x * 100
+def multiply(x):#this defines the amount of spaghetti needed for the adults
+    a = x * 100 #one adult needs about 100g of spaghetti
     return a
 
-def bmultiply(y):
-    b = y * 60
+def bmultiply(y): #this defines the amount of spaghetti needed for the children
+    b = y * 60 #one child needs about 60g of spaghetti
     return b 
 
-def add(a, b):
+def add(a, b): #sum of children and adult spaghetti
     w = a + b
     return w
 
@@ -16,8 +16,9 @@ def output(name, a, b, w):
 Hello there, {}!
 Did you know that you will need this amount of pasta in grams to cook for your people:
 {} + {} = {}
-""" .format(name, a, b, w)
+""" .format(name, a, b, w) #adding the number of pasta needed for children and adults.
 
+#defines everything the functions will do 
 def main():
     name = raw_input("Hello, what is your name?: ")
     x = raw_input("Okay. How many adults will be eating your spaghetti?: ")
