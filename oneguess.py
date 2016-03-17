e73593b5f9f592d186cbecb84d88278ddece8dba
cmis <cmis@cmis-hs16.(none)>
2016-03-17 08:40:42 +0700

    Guessing Game

---
+++ oneguess.py
@@ -0,0 +1,43 @@
+import math
+import random
+def subtract(a, b):
+	c = a - b
+	return c
+
+def correct():
+	print "You're correct"
+
+def choose(c, d):
+	chosen = random.randint(c, d)
+
+def final(a, b):
+	return "I'm thinking of a number from" + str(a) + "to" + str(b)
+ 
+def output(chosen, guess, under):
+    return """
+The target was {}
+Your guess was {}.
+That's under by {}.
+""" .format(chosen, guess, under) 
+
+def output(chosen, guess, over):
+	return """
+The target was {}
+Your guess was {}
+That's over by {}.
+""".format(chosen, guess, over) 
+def main():
+	num = raw_input("What is the minimum number?: ")
+	num2 = raw_input("What is the maximum number?: ")
+	chosen = random.randint(int(num), int(num2))
+	print final(int(num), int(num2))
+	guess = raw_input("What do you think it is?: ")
+	if int(guess) == int(chosen):
+		correct()
+	if int(guess) < int(chosen):
+		under = subtract(int(chosen), int(guess))
+		print output (chosen, guess, under)
+	if int(guess) > int(chosen):
+		over = 
+		
+main ()
