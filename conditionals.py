6b5df0694fa7bd4de64b9b9430e31cd65773d2c7
cmis <cmis@cmis-hs16.(none)>
2016-05-09 08:40:54 +0700

    down

---
+++ conditionals.py
@@ -0,0 +1,21 @@
+import math
+import random
+#This is a game about you getting lost in the middle of the forest
+#You will be asked which route to take, such as left or right?
+#You may be running into monsters to fight if you go into the wrong direction.
+#Be careful wih where you are going!
+
+def rightleft():
+	answer = raw_input("You can either take left or right, which side do you want to go for?: ")
+	if answer == "right" 
+		
+
+def starter():
+	start = raw_input("Do you want to play a game?: ")
+	if int(start) == "yes":
+		main ()
+	else:
+		print "bye" 
+
+starter()
+
--- recursion1.py
+++ recursion1.py
@@ -1,15 +1,34 @@
 import math
 import random
-def 
 
-def adder():
-	number = raw_input("Guess a number?: ") 
-	if n == "":
-		print "bye"
-	else:
-		total += int(n)
-		adder ()
+def correct():
+	print "You're correct"
+
+def choose(c, d):
+	chosen = random.randint(c, d)
+
+def final(a, b):
+	return "I'm thinking of a number from" + str(a) + "to" + str(b)
+
+def toohigh():
+	print "Your number is too high"
+
+def toolow():
+	print "Your number is too low" 
 
 def main():
+	num = raw_input("What is the minimum number?: ")
+	num2 = raw_input("What is the maximum number?: ")
+	chosen = random.randint(int(num), int(num2))
+	print final(int(num), int(num2))
+	def guess ():
+		guess = raw_input("Guess a number: ")
+		if int(guess) == int(chosen):
+			correct()
+		else:
+			guess()
+		
+main ()
+
 	
 	
