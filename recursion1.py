a3494c6f364d4a7c7526f92961843a2d289811e9
cmis <cmis@cmis-hs16.(none)>
2016-05-10 10:48:38 +0700

    Last step

--- recursion1.py
+++ recursion1.py
@@ -8,25 +8,28 @@ def choose(c, d):
 	chosen = random.randint(c, d)
 
 def final(a, b):
-	return "I'm thinking of a number from" + str(a) + "to" + str(b)
+	return "I'm thinking of a number from " + str(a) + " to " + str(b)
 
-def toohigh():
-	print "Your number is too high"
-
-def toolow():
-	print "Your number is too low" 
+def guess (chosen, time):
+	answer = raw_input("Guess a number: ")
+	if int(answer) == int(chosen):
+		correct()
+	if time == 5:
+		print "Sorry your chance is gone now. But play again!"
+	elif int(answer) < int(chosen):
+		print "Number is too low"
+		guess(chosen, time+1)
+	else:
+		print "Number is too high"
+		guess(chosen, time+1)
 
 def main():
 	num = raw_input("What is the minimum number?: ")
 	num2 = raw_input("What is the maximum number?: ")
 	chosen = random.randint(int(num), int(num2))
 	print final(int(num), int(num2))
-	def guess ():
-		guess = raw_input("Guess a number: ")
-		if int(guess) == int(chosen):
-			correct()
-		else:
-			guess()
+	time = 1
+	guess (chosen, time)
 		
 main ()
 
