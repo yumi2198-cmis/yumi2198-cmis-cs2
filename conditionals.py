6e775ec89ec21909e756ad161a9664a5fb5572f4
cmis <cmis@cmis-hs16.(none)>
2016-05-16 10:56:27 +0700

    alldone

--- conditionals.py
+++ conditionals.py
@@ -1,21 +1,87 @@
-import math
 import random
-#This is a game about you getting lost in the middle of the forest
-#You will be asked which route to take, such as left or right?
-#You may be running into monsters to fight if you go into the wrong direction.
-#Be careful wih where you are going!
+#Eat at a French restaurant with the choices of Kimbab and Soup. If you don't like any, you can leave.
 
-def rightleft():
-	answer = raw_input("You can either take left or right, which side do you want to go for?: ")
-	if answer == "right" 
+def start(hello):
+    if hello == "Hi!":
+        print "Hold on for the game!"
+
+def start2(kimchi):
+    if kimchi == "I love kimchi":
+        print "Great, that's perfect! The game will start now."
+
+def getready(car):
+    print "Dinner! Choose something to ride!"
+    if car == "Audi":
+        print "You chose the Audi."
+    elif car == "BMW":
+        print "You're bringing your BMW."
+    else:
+        car == "Toyota"
+        print "You're going to dinner in your Toyota."
+
+def pet(namess):
+    if namess == "Dog":
+        print "You're bringing a dog."
+    elif namess == "Cat":
+        print "You're bringing a cat with you."
+    else:
+        namess == "Rabbit"
+        print "You chose a rabbit."
+    
+def hunger(decision):
+    if decision == "yes":
+        print "Amazing! Welcome to the KoreaKorea Restaurant!"
+        choice = raw_input("Choose True or False to determine what you will eat: ")
+        food(choice)
+    else:
+        decision =="no"
+        print "WHY?"
+
+def food(decision2):
+    if decision2 == "True":
+        print "We will serve your kimchi right now!."
+    else:
+        decision2 == "False"
+        print "We will serve your soup now!" 
+	
 		
+def drink(drinkoption):
+    if drinkoption == "yes":
+        print "We will definitely surprise you!"
+    else:
+        drinkoption == "no"
+        print "That's not good."
 
-def starter():
-	start = raw_input("Do you want to play a game?: ")
-	if int(start) == "yes":
-		main ()
-	else:
-		print "bye" 
+def drinkchoice(surprise):
+    surprise2 = float(surprise * random.random())
+    print surprise2
+    if surprise2 > random.randint(1, 10):
+        print "You got a bottle of water!"
+    else: 
+        print "You got a soju!"
 
-starter()
+def output(car, namess):
+    return """
+You have chosen to wear the {} car and pet {}.
+ENjoy your meal!!
+""".format(car, namess)
+
+def main():
+    hello = raw_input("Type 'Hi!': ")
+    start(hello)
+    kimchi = raw_input("Type 'I love kimchi': ")
+    start2(kimchi)
+    print "Dinner! Choose something to ride!"
+    car = raw_input("Choose one of these brands to determine your car 'Toyota, BMW, Audi': ")
+    getready(car)
+    namess = raw_input("Choose one of these animals to determine your pet 'Dog, Cat, Rabbit': ")
+    pet(namess)
+    decision = raw_input("Are you hungry?: ")
+    hunger(decision)
+    decision3 = raw_input("Would u like some beverage to drink?: ")
+    drink(decision3)
+    surprise = int(raw_input("Choose a number between 1 and 10: "))
+    drinkchoice(surprise)
+    print output(car, namess) 
+main()
 
--- recursion1.py
+++ recursion1.py
@@ -12,26 +12,30 @@ def final(a, b):
 
 def guess (chosen, time):
 	answer = raw_input("Guess a number: ")
-	if int(answer) == int(chosen):
-		correct()
+	if int(answer) > int(chosen):
+		print "Number is too high"
+		guess(chosen, time+1)
 	if time == 5:
 		print "Sorry your chance is gone now. But play again!"
 	elif int(answer) < int(chosen):
 		print "Number is too low"
 		guess(chosen, time+1)
 	else:
-		print "Number is too high"
-		guess(chosen, time+1)
+		correct()
 
-def main():
+def repeat(again):
 	num = raw_input("What is the minimum number?: ")
 	num2 = raw_input("What is the maximum number?: ")
 	chosen = random.randint(int(num), int(num2))
 	print final(int(num), int(num2))
 	time = 1
-	guess (chosen, time)
-		
-main ()
+	guess(chosen, time)
 
-	
-	
+def main():
+	again = 1
+	repeat(again)
+	if int(again) <= 4:
+		repeat(again+1)
+	elif int(again) == 3:
+		print "Goodbye"
+main()
